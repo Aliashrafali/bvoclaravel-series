1dd826a02de13c3bbd06f2c4e8c98425a46e88fc
Asraf Ali <mdashraf9135@gmail.com>
2025-07-04 11:35:14 +0530

    add all new files

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,11 @@ class HomeController extends Controller
 {
     public function Index(){
         $title = 'Home Page';
-        return view('index', compact('title'));
+        $students = [
+            ["name"=>"Kisan Kumar", "age" =>22, "skill"=>"Full Stack Developer", "salary"=>15000, "status"=>0],
+            ["name"=>"Rajan Kumar", "age" =>23, "skill"=>"Laravel Developer", "salary"=>20000, "status"=>1],
+            ["name"=>"Kajal Kumari", "age" =>22, "skill"=>"JavaScript Developer", "salary"=>25000, "status"=>0]
+        ];
+        return view('index', compact('title', 'students'));
     }
 }
